db70ba3f06cd76d37a4f33e89ec88c281f3d3181
IanHazecure <136367549+IanHazecure@users.noreply.github.com>
2026-08-24 00:50:49 -0400

    screen edit

--- app/src/main/java/com/example/nutriapp/navigation/Screen.kt
+++ app/src/main/java/com/example/nutriapp/navigation/Screen.kt
@@ -1,11 +1,12 @@
 package com.example.nutriapp.navigation
 
-
 sealed class Screen(val route: String) {
     object Login : Screen("login")
     object Registro : Screen("registro")
     object RecuperarPassword : Screen("recuperar_password")
+    object Menu : Screen("menu")
     object Minuta : Screen("minuta")
+    object ListaUsuarios : Screen("lista_usuarios")
     object RecetaDetalle : Screen("receta_detalle/{recetaId}") {
         fun createRoute(recetaId: Int) = "receta_detalle/$recetaId"
     }
--- app/src/main/java/com/example/nutriapp/ui/screens/MenuScreen.kt
+++ app/src/main/java/com/example/nutriapp/ui/screens/MenuScreen.kt
@@ -1,2 +1,26 @@
 package com.example.nutriapp.ui.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.nutriapp.navigation.Screen
+
+@Composable
+fun MenuScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
